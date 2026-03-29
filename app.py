64d0832772a434bef9a85616bd2c60c0be95452b
bekoksal89-walrus <bekoksal89@gmail.com>
2026-03-29 20:18:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify"
+from flask import Flask, request, jsonify
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 from flatlib.chart import Chart
